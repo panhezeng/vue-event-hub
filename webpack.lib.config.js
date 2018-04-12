44201500254ff1f5519e09ef910f39ee512a82f6
潘何增 <panhezeng@xuetangx.com>
2018-04-12 19:18:35 +0800

    init

--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -1,14 +1,12 @@
 var path = require('path')
-var webpack = require('webpack')
 
 module.exports = {
   entry: './src/plugins/vue-event-hub.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
     filename: 'vue-event-hub.min.js',
-    library: 'VueEventHub',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
+//    library: 'VueEventHub',
+//    libraryTarget: 'umd',
+//    umdNamedDefine: true,
   },
 }
