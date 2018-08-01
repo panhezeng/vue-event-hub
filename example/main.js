d25be344a90abf922981d4fd25cdca24b69a8959
panhezeng <panhezeng@gmail.com>
2018-08-01 16:13:18 +0800

    完善文档

--- example/main.js
+++ example/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import VueEventHub from '../src/store'
+import VueEventHub from '../dist/vue-event-hub-store.min'
 import App from './components/App.vue'
 
 Vue.use(VueEventHub)
 
 new Vue({
   el: '#app',
-  render: h => h(App),
+  render: h => h(App)
 })
--- webpack.example.config.js
+++ webpack.example.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|dist/,
         use: 'babel-loader',
       },
     ],
